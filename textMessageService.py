66d2481d028009a73bd18f1a7e648c28e3436fb5
Patrick Tobin <patrick.tobin@email.wsu.edu>
2011-10-31 21:45:34 -0700

    Added some function headers to the current class... more of testing a commit of a modified file.

---
+++ textMessageService.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+'''
+    File:        textMessageService.py
+    Description: The class sends and recieves messages from email.
+                 It will translate the messages to other classes.
+    Author:      Patrick Tobin
+    Date:        10/31/2011
+'''
+class textMessageService:
+
+    def __init__(usr,pwd,phoneNumber):
+
+    # sends a message to the given phoneNumber.
+    def sendMessage(message):
+
+    # this is the method that checks the inbox of the email for any commands.
+    def recieveMessage():
