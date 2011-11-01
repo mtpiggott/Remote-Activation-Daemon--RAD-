c83344bd865bd73d91ae0f80a37a46310727cb67
Michael Piggott <michaeltpiggott@gmail.com>
2011-10-31 20:29:46 -0700

    Added Skeleton code for rad.py

---
+++ rad.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+'''
+    File:        rad.py
+    Description: Main file of the Remote Activation Daemon
+    Author:      Michael Piggott
+    Date:        10/31/2011
+'''
+def recieveMessage():
+    message = ""
+
+    return message
+
+
+def main():
+    while 1 == 1:
+        #do stuff
+        message = recieveMessage()
+
+if __name__ == '__main__':
+    main()
